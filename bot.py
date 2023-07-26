287cba44bb3de68f01696afe007873aef33f2ab8
sahil07919 <84963148+sahil07919@users.noreply.github.com>
2023-07-26 11:19:17 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -55,7 +55,7 @@ class Bot(Client):
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/CodeXBotz")
+        self.LOGGER(__name__).info(f"Bot Running..!\n\nCreated by \nhttps://t.me/DND_MOVIE")
         self.LOGGER(__name__).info(f""" DND MOVIE """)
         self.username = usr_bot_me.username
         #web-response
